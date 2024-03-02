3f5c9c07ccd76631ef7f90d7d92f658f278641ca
Omer Akram <omer@thing.com>
2024-03-02 18:52:32 +0500

    implement cbor based serializer (#4)

---
+++ test/serializers/cbor_test.dart
@@ -0,0 +1,24 @@
+import "package:test/test.dart";
+import "package:wampproto/messages.dart";
+import "package:wampproto/serializers.dart";
+
+void main() {
+  test("test serialize and deserialize", testCBOR);
+}
+
+void testCBOR() {
+  var hello = Hello.parse([1, "realm1"]);
+
+  var serializer = CBORSerializer();
+  var data = serializer.serialize(hello);
+
+  var obj = serializer.deserialize(data);
+  if (obj.messageType() != 1) {
+    fail("message");
+  }
+
+  var deserialized = obj as Hello;
+
+  expect(hello.realm, deserialized.realm);
+  expect(hello.messageType(), deserialized.messageType());
+}
