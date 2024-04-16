45b690107b4a61f749156f4b4cf159dbad02799e
Muzzammil Shahid <muzzammil@xconn.io>
2024-04-16 17:48:47 +0500

    Add wamp Hello message

---
+++ lib/src/messages/util.dart
@@ -0,0 +1,68 @@
+final Set<String> allowedRoles = <String>{"callee", "caller", "publisher", "subscriber"};
+
+void sanityCheck(List<dynamic> wampMessage, int minLength, int maxLength, int expectedId, String name) {
+  if (wampMessage.length < minLength) {
+    throw ArgumentError("invalid message length ${wampMessage.length}, must be at least $minLength");
+  }
+
+  if (wampMessage.length > maxLength) {
+    throw ArgumentError("invalid message length ${wampMessage.length}, must be at most $maxLength");
+  }
+
+  final messageId = wampMessage[0];
+  if (messageId != expectedId) {
+    throw ArgumentError("invalid message id $messageId for $name, expected $expectedId");
+  }
+}
+
+String validateStringOrRaise(Object? string, String errorMsg, String field) {
+  if (string == null) {
+    throw ArgumentError("$field cannot be null for $errorMsg");
+  }
+
+  if (string is! String) {
+    throw ArgumentError("$field must be of type string for $errorMsg");
+  }
+
+  return string;
+}
+
+Map<String, dynamic> validateMapOrRaise(Object? map, String errorMsg, String field) {
+  if (map == null) {
+    throw ArgumentError("$field cannot be null for $errorMsg");
+  }
+
+  if (map is! Map<String, dynamic>) {
+    throw ArgumentError("$field must be of type map for $errorMsg");
+  }
+
+  return map;
+}
+
+List<String> validateListOrRaise(Object? list, String errorMsg, String field) {
+  if (list == null) {
+    throw ArgumentError("$field cannot be null for $errorMsg");
+  }
+
+  if (list is! List<String>) {
+    throw ArgumentError("$field must be of type list for $errorMsg");
+  }
+  return list;
+}
+
+Map<String, Map<String, bool>> validateRolesOrRaise(Object? roles, String errorMsg) {
+  if (roles == null) {
+    throw ArgumentError("roles cannot be null for $errorMsg");
+  }
+
+  if (roles is! Map<String, Map<String, bool>>) {
+    throw ArgumentError("roles must be of type map for $errorMsg");
+  }
+
+  for (final role in roles.keys) {
+    if (!allowedRoles.contains(role)) {
+      throw ArgumentError("invalid role '$role' in 'roles' details for $errorMsg");
+    }
+  }
+  return roles;
+}
