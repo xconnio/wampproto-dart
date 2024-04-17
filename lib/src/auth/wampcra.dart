bca1582f2f5db34d59ddf46739d1823219ad9811
Muzzammil Shahid <muzzammil@xconn.io>
2024-04-17 19:02:43 +0500

    Add helpers to generate and verify CRA challenge

--- lib/src/auth/wampcra.dart
+++ lib/src/auth/wampcra.dart
@@ -1,4 +1,5 @@
 import "dart:convert";
+import "dart:math";
 import "dart:typed_data";
 
 import "package:crypto/crypto.dart";
@@ -18,6 +19,38 @@ class WAMPCRAAuthenticator extends IClientAuthenticator {
   }
 }
 
+String generateNonce() {
+  final random = Random.secure();
+  final nonceBytes = Uint8List.fromList(List.generate(16, (_) => random.nextInt(256)));
+  return nonceBytes.map((byte) => byte.toRadixString(16).padLeft(2, "0")).join();
+}
+
+String utcNow() {
+  DateTime dt = DateTime.now().toUtc();
+  return "${dt.toIso8601String().substring(0, 23)}Z";
+}
+
+String generateWampcraChallenge(int sessionId, String authId, String authRole, String provider) {
+  final nonce = generateNonce();
+
+  final data = {
+    "nonce": nonce,
+    "authprovider": provider,
+    "authid": authId,
+    "authrole": authRole,
+    "authmethod": "wampcra",
+    "session": sessionId,
+    "timestamp": utcNow,
+  };
+
+  return json.encode(data);
+}
+
 String signWampCRAChallenge(String challenge, Uint8List key) {
   return Hmac(sha256, key).convert(utf8.encode(challenge)).toString();
+}
+
+bool verifyWampCRASignature(String signature, String challenge, Uint8List key) {
+  final localSignature = signWampCRAChallenge(challenge, key);
+  return signature == localSignature;
 }
