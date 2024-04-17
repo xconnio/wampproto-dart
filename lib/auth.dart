1e586ee6091851bb9f0ebe6e000bd22161526fb9
Mahad <mahad@xconn.io>
2024-04-17 16:15:09 +0500

    Add Anonymous Authenticator

---
+++ lib/auth.dart
@@ -0,0 +1,1 @@
+export "src/auth/anonymous.dart" show AnonymousAuthenticator;
---
+++ lib/src/auth/auth.dart
@@ -0,0 +1,16 @@
+import "package:wampproto/messages.dart";
+
+abstract class IClientAuthenticator {
+  IClientAuthenticator(String method, String authID, [Map<String, dynamic>? authExtra])
+      : _method=method, _authID=authID, _authExtra = authExtra ?? <String, dynamic>{};
+
+  final String _method;
+  final String _authID;
+  final Map<String, dynamic>? _authExtra;
+
+  String get authMethod => _method;
+  String get authID => _authID;
+  Map<String, dynamic>? get authExtra => _authExtra;
+
+  Authenticate authenticate(Challenge challenge);
+}
