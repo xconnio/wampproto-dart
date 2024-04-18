fb94870427d2786c9b02b06060f1ade2b6796f6e
Mahad <mahad@xconn.io>
2024-04-18 16:03:25 +0500

    Add CryptoSign Authenticator (#29)

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,3 +1,4 @@
 export "src/auth/anonymous.dart" show AnonymousAuthenticator;
 export "src/auth/ticket.dart" show TicketAuthenticator;
 export "src/auth/wampcra.dart" show WAMPCRAAuthenticator;
+export "src/auth/cryptosign.dart" show CryptoSignAuthenticator;
