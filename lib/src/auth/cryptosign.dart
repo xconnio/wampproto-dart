aa5d1aca47606a1078af79d5b5a3ca1573483387
Mahad <mahad@xconn.io>
2024-04-18 15:59:56 +0500

    Add CryptoSign Authenticator

--- lib/src/auth/cryptosign.dart
+++ lib/src/auth/cryptosign.dart
@@ -1,7 +1,38 @@
 import "dart:math";
+
 import "package:pinenacl/ed25519.dart";
+import "package:wampproto/messages.dart";
+import "package:wampproto/src/auth/auth.dart";
 
 const hex = Base16Encoder.instance;
+
+class CryptoSignAuthenticator extends IClientAuthenticator {
+  CryptoSignAuthenticator(String authID, this._privateKeyHex, [Map<String, dynamic>? authExtra])
+      : super(type, authID, authExtra) {
+    if (authExtra == null || !authExtra.containsKey("pubkey")) {
+      final pubKey = getPrivateKey().verifyKey;
+      authExtra?["pubkey"] = pubKey;
+    }
+  }
+
+  static const String type = "cryptosign";
+  final String _privateKeyHex;
+
+  SigningKey getPrivateKey() {
+    return SigningKey(seed: hex.decode(_privateKeyHex));
+  }
+
+  @override
+  Authenticate authenticate(Challenge challenge) {
+    if (!challenge.extra.containsKey("challenge")) {
+      throw ArgumentError("challenge string missing in extra");
+    }
+    var challengeHex = challenge.extra["challenge"];
+    var signed = signCryptoSignChallenge(challengeHex, getPrivateKey());
+
+    return Authenticate(signed + challengeHex, {});
+  }
+}
 
 String generateCryptoSignChallenge() {
   List<int> rawBytes = List<int>.generate(32, (index) => Random().nextInt(256));
