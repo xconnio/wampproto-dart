050ff750055e65054b22f036b9b2c82f66c47f8e
Muzzammil Shahid <muzzammil@xconn.io>
2024-04-19 13:44:10 +0500

    Add wamp call message

---
+++ lib/src/messages/call.dart
@@ -0,0 +1,66 @@
+import "package:wampproto/src/messages/message.dart";
+import "package:wampproto/src/messages/util.dart";
+
+class Call implements Message {
+  Call(
+    this.requestID,
+    this.uri, {
+    List<dynamic>? args,
+    Map<String, dynamic>? kwargs,
+    Map<String, dynamic>? options,
+  })  : args = args ?? [],
+        kwargs = kwargs ?? {},
+        options = options ?? {};
+
+  static const int id = 48;
+
+  static const String text = "CALL";
+
+  final int requestID;
+  final String uri;
+  final List<dynamic> args;
+  final Map<String, dynamic> kwargs;
+  final Map<String, dynamic> options;
+
+  static Call parse(final List<dynamic> message) {
+    sanityCheck(message, 4, 6, id, text);
+
+    int requestID = validateIntOrRaise(message[1], text, "request ID");
+
+    Map<String, dynamic> options = validateMapOrRaise(message[2], text, "options");
+
+    String uri = validateStringOrRaise(message[3], text, "uri");
+
+    List<dynamic>? args;
+    if (message.length > 4) {
+      args = validateListOrRaise(message[4], text, "args");
+    }
+
+    Map<String, dynamic>? kwargs;
+    if (message.length > 5) {
+      kwargs = validateMapOrRaise(message[5], text, "kwargs");
+    }
+
+    return Call(requestID, uri, args: args, kwargs: kwargs, options: options);
+  }
+
+  @override
+  List<dynamic> marshal() {
+    List<dynamic> message = [id, requestID, options, uri];
+    if (args.isNotEmpty) {
+      message.add(args);
+    }
+
+    if (kwargs.isNotEmpty) {
+      if (args.isEmpty) {
+        message.add([]);
+      }
+      message.add(kwargs);
+    }
+
+    return message;
+  }
+
+  @override
+  int messageType() => id;
+}
