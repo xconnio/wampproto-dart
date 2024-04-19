bfacbacd21f3567e770a1086e98df63f2216b50c
Muzzammil Shahid <muzzammil@xconn.io>
2024-04-19 13:12:10 +0500

    Add wamp Subscribe message

---
+++ lib/src/messages/subscribe.dart
@@ -0,0 +1,34 @@
+import "package:wampproto/src/messages/message.dart";
+import "package:wampproto/src/messages/util.dart";
+
+class Subscribe implements Message {
+  Subscribe(this.requestID, this.topic, {Map<String, dynamic>? options}) : options = options ?? {};
+
+  static const int id = 32;
+
+  static const String text = "SUBSCRIBE";
+
+  final int requestID;
+  final int topic;
+  final Map<String, dynamic> options;
+
+  static Subscribe parse(final List<dynamic> message) {
+    sanityCheck(message, 4, 4, id, text);
+
+    int requestID = validateIntOrRaise(message[1], text, "request ID");
+
+    Map<String, dynamic> options = validateMapOrRaise(message[2], text, "options");
+
+    int topic = validateIntOrRaise(message[3], text, "topic");
+
+    return Subscribe(requestID, topic, options: options);
+  }
+
+  @override
+  List<dynamic> marshal() {
+    return [id, requestID, options, topic];
+  }
+
+  @override
+  int messageType() => id;
+}
