aee9ebc23a86842509695d58f93f9e5bd8e39145
Mahad <mahad@xconn.io>
2024-04-22 18:24:00 +0500

    serializer all messages (#38)

--- lib/src/serializers/serializer.dart
+++ lib/src/serializers/serializer.dart
@@ -2,8 +2,6 @@ import "dart:typed_data";
 
 import "package:wampproto/messages.dart";
 
-import "package:wampproto/src/messages/message.dart";
-
 Message toMessage(List<dynamic> message) {
   var messageType = message[0];
   if (messageType is! int) {
@@ -15,6 +13,87 @@ Message toMessage(List<dynamic> message) {
       {
         return Hello.parse(message);
       }
+    case Welcome.id:
+      {
+        return Welcome.parse(message);
+      }
+    case Abort.id:
+      {
+        return Abort.parse(message);
+      }
+    case Challenge.id:
+      {
+        return Challenge.parse(message);
+      }
+    case Authenticate.id:
+      {
+        return Authenticate.parse(message);
+      }
+    case Goodbye.id:
+      {
+        return Goodbye.parse(message);
+      }
+    case Call.id:
+      {
+        return Call.parse(message);
+      }
+    case Invocation.id:
+      {
+        return Invocation.parse(message);
+      }
+    case Yield.id:
+      {
+        return Yield.parse(message);
+      }
+    case Result.id:
+      {
+        return Result.parse(message);
+      }
+    case Register.id:
+      {
+        return Register.parse(message);
+      }
+    case Registered.id:
+      {
+        return Registered.parse(message);
+      }
+    case UnRegister.id:
+      {
+        return UnRegister.parse(message);
+      }
+    case UnRegistered.id:
+      {
+        return UnRegistered.parse(message);
+      }
+    case Subscribe.id:
+      {
+        return Subscribe.parse(message);
+      }
+    case Subscribed.id:
+      {
+        return Subscribed.parse(message);
+      }
+    case UnSubscribe.id:
+      {
+        return UnSubscribe.parse(message);
+      }
+    case UnSubscribed.id:
+      {
+        return UnSubscribed.parse(message);
+      }
+    case Publish.id:
+      {
+        return Publish.parse(message);
+      }
+    case Published.id:
+      {
+        return Published.parse(message);
+      }
+    case Event.id:
+      {
+        return Event.parse(message);
+      }
+
     default:
       {
         throw "unknown message type";
