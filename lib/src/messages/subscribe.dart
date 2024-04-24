256d109c752fdf1150bd9c4a317bfc0112b22ecb
Mahad <mahad@xconn.io>
2024-04-24 13:13:58 +0500

    change topic type to string in Subscribe

--- lib/src/messages/subscribe.dart
+++ lib/src/messages/subscribe.dart
@@ -9,7 +9,7 @@ class Subscribe implements Message {
   static const String text = "SUBSCRIBE";
 
   final int requestID;
-  final int topic;
+  final String topic;
   final Map<String, dynamic> options;
 
   static Subscribe parse(final List<dynamic> message) {
@@ -19,7 +19,7 @@ class Subscribe implements Message {
 
     Map<String, dynamic> options = validateMapOrRaise(message[2], text, "options");
 
-    int topic = validateIntOrRaise(message[3], text, "topic");
+    String topic = validateStringOrRaise(message[3], text, "topic");
 
     return Subscribe(requestID, topic, options: options);
   }
